a74de9c125895c8cabdfdfb7de0dfdc8f3c11dfe
SeikhSawan <ssawan2008@gmail.com>
2026-05-06 10:29:18 -0400

    FIX ROSTER

--- src/Roster.java
+++ src/Roster.java
@@ -1,12 +1,13 @@
 public class Roster {
+    private int studentID;
     private int rosterID;
     private Roster roster;
 
     public Roster(Roster roster, String student, int studentID) {
         this.rosterID = rosterID;
-         this.roster = roster;
-
-         int arr1 = Integer.parseInt((String) student);
+        this.roster = roster;
+        this.studentID = studentID;
+        int arr1 = Integer.parseInt((String) student);
     }
 
     public int getRosterID() {
