43b727ace4360521fe9a34c2afa8654193b599b6
Kerui Chen <keruic3@nycstudents.net>
2026-05-12 10:17:14 -0400

    created Populating script for departments

---
+++ src/DepartmentCreator.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class DepartmentCreator{
+    public static final String[] DEPARTMENTS = {"Biology", "Math", "ELA", "Chemistry", "CS"};
+    private ArrayList<Departments> refrencesList = new ArrayList<>();
+    public DepartmentCreator(){
+        for(String s : DEPARTMENTS){
+            refrencesList.add(new Departments(s));
+        }
+    }
+    @Override
+    public String toString() {
+        String total_concat = "";
+        for(String s : DEPARTMENTS){
+            total_concat += "INSERT INTO Rooms (departmentName) VALUES " + "(\""+s+"\")" + ";\n";
+        }
+        return total_concat;
+    }
+}
--- src/StudentSchedule.java
+++ src/StudentSchedule.java
@@ -5,11 +5,16 @@ public class StudentSchedule {
     private Students students;
 
     public StudentSchedule (Students students, Offer[] offer) {
+        offeringID = new int[offer.length];
         for (int offerIndex = 0; offerIndex < offer.length; offerIndex++) {
             offeringID[offerIndex] = offer[offerIndex].getOfferingID();
         }
         this.students = students;
         studentID = students.getStudentID();
+    }
+
+    public int[] getOfferingID() {
+        return offeringID;
     }
 }
 // in bash, we will this and teacherschedule with sets but how can we use
