ccd86230406d2ee2662d6de68dd6a639f905a5b1
JoshuaSagalchik <joshuas697@nycstudents.net>
2026-05-20 09:51:21 -0400

    fixed Offering

--- src/Offering.java
+++ src/Offering.java
@@ -1,13 +1,57 @@
 public class Offering {
-    private int offeringID;
-    private static int counter = 0;
+    private int period;
+    private int offeringID = 0;
+    private Assignment[] assignments;
+    private int[] assignmentID;
+    private Teachers teacher;
+    private int teacherID;
+    static int counter = 1;
+    private int rosterID;
+    private Course course;
+    private int courseID;
 
-    public Offering() {
-        offeringID = ++counter;
+
+    public Offering(int period, Assignment[] assignments, Course course, Teachers teacher, Roster roster) {
+        this.period = period;
+        this.assignments = assignments;
+        int count = 0;
+        for (Assignment assignment : assignments) {
+            assignmentID[count] = assignment.getAssignmentID();
+            count++;
+        }
+        teacherID = teacher.getTeacherID();
+        this.teacher = teacher;
+        rosterID = roster.getRosterID();
+        offeringID = counter;
+        counter++;
+        courseID = course.getCourseID();
+        this.course = course;
     }
+
 
     public int getOfferingID() {
         return offeringID;
     }
+
+
+    public int getPeriod() {
+        return period;
+    }
+
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+
+    public Assignment[] getAssignments() {
+        return assignments;
+    }
+
+
+    public int[] getAssignmentID() {
+        return assignmentID;
+    }
 }
+
 
