b8cd34df76e3bfc5c406e5e87cd4e591d79067cc
SeikhSawan <ssawan2008@gmail.com>
2026-05-20 09:58:01 -0400

    oo

---
+++ src/StudentPopulation2.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class StudentPopulation2 {
+    public static <Student> void studentsPopulatingScript() {
+        int count = 0;
+        Path path = Paths.get("src/names.txt");
+        try {
+            List<String> lines = Files.readAllLines(path);
+//            Collections.sort(lines);
+//            String currentLine = String.join("\n", lines);
+            List<Student> studentList = new ArrayList<>();
+            for (String name : lines) {
+                if (!name.trim().isEmpty()) {
+                    studentList.add((Student) new Students(name));
+                }
+            }
+            for (Student s : studentList) {
+                count++;
+                System.out.println(s);
+            }
+            System.out.println(count);
+        } catch (IOException e) {
+            System.out.println("Can't find the file.");
+        } catch (Exception e) {
+            System.out.println("Some weird error occurred. I can't handle ts");
+        }
+    }
+}
+
+
--- src/Students.java
+++ src/Students.java
@@ -30,31 +30,9 @@ public class Students {
     //For the student population script, make a loop that calls the student constructor and for the studentName parameter you should get it from a file with all 5000 studentNames
     @Override
     public String toString() {
-        return "INSERT INTO Students (studentName) VALUES (" + getStudentName() + ")";
+        return "INSERT INTO Students (studentName) VALUES (\'" + getStudentName() + "\')";
 
     }
 //    (Stream<String> stream = Files.lines(Paths.get("names.txt")))
 
-    public static class StudentPopulation2 {
-        public static <Student> void main(String[] args) {
-            Path path = Paths.get("src/names.txt");
-            try (Scanner readFile = new Scanner(path)) {
-                List<String> lines = Files.readAllLines(Paths.get(path.toUri()));
-                Collections.sort(lines);
-                String currentLine = String.join("\n", lines);
-                List<Student> students = new ArrayList<>();
-                for (String Lines : lines) {
-                    Student s = new Student();
-                    students.add(s);
-                }
-                for (Student s : students) {
-                    System.out.println(s);
-                }
-            } catch (IOException e) {
-                System.out.println("Can't find the file.");
-            } catch (Exception e) {
-                System.out.println("Some weird error occurred. I can't handle ts");
-            }
-        }
-    }
 }
