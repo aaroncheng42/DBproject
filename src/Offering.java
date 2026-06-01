29fb96d5bf01bff7f7e90caa43b4647a0e61d0c8
KeruiChen <keruic3@nycstudents.net>
2026-06-01 10:11:18 -0400

    made both sceduals into offerings

--- src/Offering.java
+++ src/Offering.java
@@ -34,6 +34,7 @@ public class Offering {
         for(Student s : this.students){
             inital += "INSERT INTO Rosters (offeringID, studentsID) VALUES ("+offeringID +", "+ s.getStudentID() +")\n";
         }
+        inital += "INSERT INTO TeacherSchedule  (offeringID, teacherID) VALUES ("+offeringID +", "+ teacher.getTeacherID() +")\n";
         inital += "INSERT INTO Offering (period, roomID, courseID, teacherID) VALUES ("+period +", "+ room.getRoomID() +", "+ course.getCourseID() +", "+ teacher.getTeacherID()+")\n";
         return inital;
     }
